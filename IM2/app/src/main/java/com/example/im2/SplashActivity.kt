dc4c1cbb89fde4213f63b31e727d89c2d9d46aae
fzu221801328 <962900904@qq.com>
2020-04-15 16:14:26 +0800

    splash界面实现

---
+++ IM2/app/src/main/java/com/example/im2/SplashActivity.kt
@@ -0,0 +1,8 @@
+package com.example.im2
+
+import com.example.homeworkplatform.BaseActivity
+
+class SplashActivity:BaseActivity() {
+    override fun getLayoutResId(): Int
+       = R.layout.splash
+}
