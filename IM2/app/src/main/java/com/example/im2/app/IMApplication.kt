a81bd285f99f21b49bca4fc6892d1fa1a1c3bf0b
fzu221801328 <962900904@qq.com>
2020-04-18 21:21:40 +0800

    登陆界面布局补充

--- IM2/app/src/main/java/com/example/im2/app/IMApplication.kt
+++ IM2/app/src/main/java/com/example/im2/app/IMApplication.kt
@@ -9,8 +9,7 @@ class IMApplication:Application() {
     override fun onCreate() {
         super.onCreate()
 
-
-//初始化
+//环信sdk初始化
         EMClient.getInstance().init(applicationContext,EMOptions());
 //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(BuildConfig.DEBUG);
