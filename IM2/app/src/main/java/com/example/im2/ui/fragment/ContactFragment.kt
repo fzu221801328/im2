8a03dbaa328ef7769487416c826a81ecf729caca
fzu221801328 <962900904@qq.com>
2020-05-06 20:50:03 +0800

    联系人界面布局初始化

--- IM2/app/src/main/java/com/example/im2/ui/fragment/ContactFragment.kt
+++ IM2/app/src/main/java/com/example/im2/ui/fragment/ContactFragment.kt
@@ -1,9 +1,26 @@
 package com.example.im2.ui.fragment
 
+import android.view.View
 import com.example.homeworkplatform.BaseFragment
 import com.example.im2.R
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 
 class ContactFragment:BaseFragment() {
     override fun getLayoutResId(): Int
         = R.layout.fragment_contacts
+
+    override fun init() {
+        super.init()
+        /*设置标题*/
+        headerTitle.text = getString(R.string.contact)
+        /*设置+号可见*/
+        add.visibility = View.VISIBLE
+        /*设置下拉刷新颜色不然是黑色*/
+        swipeRefreshLayout.apply {
+            setColorSchemeResources(R.color.qq_blue)
+            isRefreshing = true
+        }
+
+    }
 }
