12f301c9cc125c28ba4755327c08e568fa557ea5
fzu221801328 <962900904@qq.com>
2020-05-28 09:47:10 +0800

    发送一条消息view层实现

--- IM2/app/src/main/java/com/example/im2/ui/activity/ChatActivity.kt
+++ IM2/app/src/main/java/com/example/im2/ui/activity/ChatActivity.kt
@@ -6,10 +6,12 @@ import android.util.Log
 import android.view.View
 import com.example.homeworkplatform.BaseActivity
 import com.example.im2.R
+import com.example.im2.contract.ChatContract
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.toast
 
-class ChatActivity:BaseActivity() {
+class ChatActivity:BaseActivity(),ChatContract.View {
 
     val TAG = "ChatActivity"
 
@@ -54,5 +56,23 @@ class ChatActivity:BaseActivity() {
         Log.d(TAG,"获取聊天的用户名$username")
         val titleString = String.format(getString(R.string.chat_title),username)
         headerTitle.text = titleString
+    }
+
+    override fun onStartSendMessage() {
+        //通知recyclerview刷新列表，显示一条正在发送的消息
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onSendMessageSuccess() {
+        recyclerView.adapter?.notifyDataSetChanged()
+        toast(R.string.send_message_success)
+        //清空编辑框
+        edit.text.clear()
+    }
+
+    override fun onSendMessageFailed() {
+        //加载变成出错
+        recyclerView.adapter?.notifyDataSetChanged()
+        toast(R.string.send_message_failed)
     }
 }
