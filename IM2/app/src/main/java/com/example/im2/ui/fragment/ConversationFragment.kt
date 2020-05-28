86efdbd15496bac405610d2be58a2c21a45f1ce1
fzu221801328 <962900904@qq.com>
2020-05-28 18:40:34 +0800

    消息界面布局

--- IM2/app/src/main/java/com/example/im2/ui/fragment/ConversationFragment.kt
+++ IM2/app/src/main/java/com/example/im2/ui/fragment/ConversationFragment.kt
@@ -2,8 +2,14 @@ package com.example.im2.ui.fragment
 
 import com.example.homeworkplatform.BaseFragment
 import com.example.im2.R
+import kotlinx.android.synthetic.main.header.*
 
 class ConversationFragment:BaseFragment() {
     override fun getLayoutResId(): Int
         = R.layout.fragment_conversation
+
+    override fun init() {
+        super.init()
+        headerTitle.text = getString(R.string.message)
+    }
 }
